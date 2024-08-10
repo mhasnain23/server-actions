6626c0d92bf7795b5403f5968b6cc7c1179fa736
mhasnain23 <tryfrelancer23@gmail.com>
2024-08-11 01:00:12 +0500

    nit

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <div className="min-h-full flex justify-center mt-[20rem]">
+        <div className="min-h-full flex justify-center mt-[10rem]">
           <Hero />
         </div>
         <div class="absolute inset-0 -z-10 h-full w-full bg-black bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
