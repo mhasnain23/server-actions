e3e1d100d7e00c1be12e0ff2c7e8c8ee792c1302
mhasnain23 <tryfrelancer23@gmail.com>
2024-08-11 10:48:16 +0500

    nit

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+
+
+const About = () => {
+    return (
+        <>
+            <div className="max-w-7xl mx-auto h-full items-center px-10 py-16">
+                <h1 className="text-[#ffffffda] text-3xl font-semibold">About</h1>
+                <p className="font-mono text-xl text-[#ffffffda] mt-5">Our User Management App offers a fast, efficient way to handle user accounts. With streamlined CRUD operations and a minimalistic UI, you can manage users swiftly and without distractions. The app is designed for performance, ensuring quick response times even with large datasets. Whether you're adding, editing, or removing users, the process is intuitive and hassle-free. Plus, secure authentication and role-based access control keep your system protected while allowing you to control who has access to what.</p>
+            </div>
+            <div class="absolute inset-0 -z-10 h-full w-full bg-black bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
+        </>
+    )
+}
+
+export default About
